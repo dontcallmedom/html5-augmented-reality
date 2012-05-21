24169fcf382eca35fea76cd4f511fd38154337f0
Dominique Hazael-Massieux <dom@w3.org>
2012-05-21 17:28:11 +0200

    rougly working

--- js/script.js
+++ js/script.js
@@ -1,6 +1,7 @@
 jQuery(document).ready(function ($) {
  var cameraOutput = document.getElementById("camera");
  var poi = [];
+ var maxDistance = 0;
  function gotCameraStream(stream) {
      cameraOutput.src = stream;
      cameraOutput.play();     
@@ -15,32 +16,44 @@ jQuery(document).ready(function ($) {
      $.getJSON("js/data.json", function(data) {
        for (var i = 0 ; i < data.length; i++) {
 	   var p = projectedPOI(here,data[i]);
-           console.log(p);
+           maxDistance = Math.max(maxDistance, p.distance);
 	   poi.push(p);
        }
+   for (var i =0 ; i<poi.length; i++) {
+     console.log(poi[i].distance + " " + maxDistance);
+     console.log( (poi[i].distance/maxDistance) * (240-15));
+   }
      });
-
  }
  if (navigator.getUserMedia) {
     navigator.getUserMedia({video:true}, gotCameraStream, errorWithCamera);     
  }
  navigator.geolocation.getCurrentPosition(gotPosition);
  var ctx = document.getElementById("overlay").getContext("2d");
- ctx.fillStyle= "rgba(0,0,0,0.5)";
- ctx.fillRect(0,100,320,40);
- ctx.fillStyle = "rgba(255,255,255,255)";
  ctx.textAlign = "center";
  ctx.textBaseline = "middle";
- ctx.strokeStyle = "black";  
  ctx.font="15px Arial";
  window.addEventListener("deviceorientation", function(e) {
    $("#orientation").html(e.alpha);
-   ctx.clearRect(0,100,320,140);
-   ctx.fillStyle= "rgba(0,0,0,0.5)";
-   ctx.fillRect(0,100,320,40);
-   ctx.fillStyle = "rgba(255,255,255,255)";
+   ctx.clearRect(0,0,320,240);
    for (var i =0 ; i<poi.length; i++) {
-     ctx.fillText(poi[i].label,160 + ((e.alpha - poi[i].angle) % 360)*16/9,120);
+     //ctx.fillStyle= "rgba(0,0,0,0.5)";
+     //ctx.fillRect(0,100,320,40);
+     ctx.fillStyle = "white";
+     console.log(poi[i].distance + " " + maxDistance);
+     // Based on direction of POI
+     var x = 160 + ((360 + e.alpha - poi[i].angle) % 360)*16/9;
+     // Put text based on logarithmic scale of distance
+     var y = 240 - Math.log(poi[i].distance) / Math.log(Math.pow(maxDistance, 1/ (240-15)));
+     ctx.beginPath();
+     ctx.moveTo(160,260);
+     ctx.lineTo(x,y);
+     ctx.strokeStyle = "white";
+     ctx.stroke();
+     ctx.font="10px Arial";
+     ctx.fillText(Math.floor(poi[i].distance / 100) / 10 + 'km',160 + (x - 160) / 2, 240 + (y - 240)/2);
+     ctx.font="15px Arial";
+     ctx.fillText(poi[i].label,x,y);
    }
  });
 });
