011737c7e344017925a9cc3a39ba89ea51b41c12
Dominique Hazael-Massieux <dom@w3.org>
2012-05-21 13:09:00 +0200

    indicating cardinal directions

--- js/script.js
+++ js/script.js
@@ -10,8 +10,18 @@ jQuery(document).ready(function ($) {
  }
 
  navigator.getUserMedia({video:true}, gotCameraStream, errorWithCamera);
-
+ var ctx = document.getElementById("overlay").getContext("2d");
+ ctx.fillStyle = "white";
+ ctx.textAlign = "center";
+ ctx.textBaseline = "middle";
+ ctx.strokeStyle = "black";  
+ ctx.font="15px Arial";
  window.addEventListener("deviceorientation", function(e) {
    $("#orientation").html(e.alpha);
+   ctx.clearRect(0,0,320,240);
+   ctx.fillText("South",160 + ((e.alpha - 180) % 360)*16/9,120);
+   ctx.fillText("North",160 + (e.alpha % 360)*16/9,120);
+   ctx.fillText("East",160 + ((e.alpha - 90) % 360)*16/9,120);
+   ctx.fillText("West",160 + ((e.alpha - 270) % 360)*16/9,120);
  });
 });
