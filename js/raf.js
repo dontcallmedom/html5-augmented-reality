219bb22d5ebcec4505bc98e583ae125bd4f9d049
Dominique Hazael-Massieux <dom@w3.org>
2013-02-28 08:24:58 +0100

    requestanimationframe shim

---
+++ js/raf.js
@@ -0,0 +1,31 @@
+/* START: requestAnimationFrame shim
+ *
+ * By: Eric Möller
+ * URL: http://my.opera.com/emoller/blog/2011/12/20/requestanimationframe-for-smart-er-animating
+ */
+ (function() {
+    var lastTime = 0;
+    var vendors = ['ms', 'moz', 'webkit', 'o'];
+    for (var x = 0; x < vendors.length && !window.requestAnimationFrame; ++x) {
+        window.requestAnimationFrame = window[vendors[x] + 'RequestAnimationFrame'];
+        window.cancelRequestAnimationFrame = window[vendors[x] +
+        'CancelRequestAnimationFrame'];
+    }
+
+    if (!window.requestAnimationFrame)
+    window.requestAnimationFrame = function(callback, element) {
+        var currTime = new Date().getTime();
+        var timeToCall = Math.max(0, 16 - (currTime - lastTime));
+        var id = window.setTimeout(function() {
+            callback(currTime + timeToCall);
+        },
+        timeToCall);
+        lastTime = currTime + timeToCall;
+        return id;
+    };
+
+    if (!window.cancelAnimationFrame)
+    window.cancelAnimationFrame = function(id) {
+        clearTimeout(id);
+    };
+} ());
